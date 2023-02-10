31e612a535e73b5a8743ca1048a40b60ed302b3d
Aniket Pratap <aniketpx@gmail.com>
2023-02-10 14:11:08 -0800

    parsing attempt

--- indexer.py
+++ indexer.py
@@ -2,27 +2,30 @@ import json
 from bs4 import BeautifulSoup
 import cbor
 
+check = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+wordList = []
+
 def createIndex():
-    #variables
+    # variables
     allTokens = []
-    parentDirectory = r"C:\Users\farme\Downloads\webpages\WEBPAGES_RAW"
+    parentDirectory = "/Users/aniketpratap/Desktop/WEBPAGES_RAW"
 
-    #open file
-    f = open(r"C:\Users\farme\Downloads\webpages\WEBPAGES_RAW\bookkeeping.json")
+    # open file
+    f = open("/Users/aniketpratap/Desktop/WEBPAGES_RAW/bookkeeping.json")
 
-    #load json
+    # load json
     data = json.load(f)
 
-    #create soup object
+    # create soup object
     soup = BeautifulSoup()
 
-    #loop through and open files
+    # loop through and open files
     for entry in data:
         location = entry.split("/")
         url = data[entry]
 
-        #create path for file
-        path = parentDirectory + "\\" + location[0] + "\\" + location[1]
+        # create path for file
+        path = parentDirectory + "/" + location[0] + "/" + location[1]
 
         # data_dict = cbor.load(open(path, "rb"))
 
@@ -30,10 +33,24 @@ def createIndex():
 
         # print(content)
 
-        with open(path, "r") as website:
-            print(website)
-        
+        with open(path, 'r') as website:
 
-        
+            word = ''
+            for content in website:
+                soup = BeautifulSoup(content, 'html.parser')
+                based = soup.find_all('div')
+                for i in soup:
+                    for c in i.text:
+                        if c not in check and len(word) > 1:
+                            # wordList.append(word)
+                            print(word, path)
+                            word = ''
+                        if c in check:
+                            word += c.lower()
+                        if c == "":
+                            # wordFreq[word] = 1 + wordFreq.get(word, 0)
+                            # wordCount += 1
+                            break
+
 
 createIndex()
