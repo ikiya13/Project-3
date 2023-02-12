d67ab09e8039d09bb138299ef8b8c149fefd24f3
Kiyarash Khanessari <kiyakhanesari@gmail.com>
2023-02-12 15:55:18 -0800

    Adds saving to .pickle file.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #import required libraries
 import sys
+import pickle
 
 #other modules
 from indexer import createIndex
@@ -7,3 +8,8 @@ from indexer import createIndex
 #call indexer passing command line argument
 index = createIndex(sys.argv[1])
 
+#save to file
+pickle.dump(index, open("indexFile.pickle", "wb"))
+
+#read from file
+index = pickle.load(open("indexFile.pickle", "rb"))
