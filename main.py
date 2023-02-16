39bf73d12b49d30009d576374b8c934cb2b0f35a
akbenothman <akbenothman@gmail.com>
2023-02-15 16:56:41 -0800

    added more analytics for M1

--- main.py
+++ main.py
@@ -27,6 +27,14 @@ else:
     index = pickle.load(open("indexFile.pickle", "rb"))
     #print(index)
 
+# Analytics
+doc_ID = set()
+for token, layer1 in index.items():
+    for url in layer1:
+        doc_ID.add(url)
+
+logging.info(f"Number of unique document id's in index: {len(doc_ID)}")
+logging.info(f"Number of unique words in index: {len(index)}")
 
 # input search, split 1 or 2 keywords, call searchIndex with list of tokens (1or2)
 search = input("Search 1 or 2 keywords: ").lower()
